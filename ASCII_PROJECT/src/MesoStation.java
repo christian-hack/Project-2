a87adf68ad556874f0d0a95eb2f5893995858307
Christian Hackelman <cphackelman@ou.edu>
2019-03-07 20:37:32 -0600

    New information

--- ASCII_PROJECT/src/MesoStation.java
+++ ASCII_PROJECT/src/MesoStation.java
@@ -12,3 +12,5 @@ public class MesoStation
 		return StID;
 	}
 }
+//"A Million Times" seems to be the story of how T-Pain is really down for whatever a girl he's pursuing is into.
+//He doesn't seem to go too far into detail as to what the extent of this is. Like is he REALLY down for whatever??'
